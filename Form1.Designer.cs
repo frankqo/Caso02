feda3bea1eaede1a25c76c2a79079bd9cde2e43d
PC-USER <PC-USER@DESKTOP-NQSUID8>
2021-07-31 18:10:48 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,412 @@
+﻿
+namespace Caso01
+{
+    partial class frmLlamdasSinValorRetornoConParametros
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lblCosto = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cboHorario = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lvRegistro = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnEstadisticas = new System.Windows.Forms.Button();
+            this.lvEstadisticas = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tHora = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Control de Registro de Llamadas-Call Center";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 78);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del Terminalista";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(9, 32);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(175, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre Completo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblHora);
+            this.groupBox2.Controls.Add(this.lblFecha);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(221, 43);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(203, 78);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos de Registro";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(129, 39);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(40, 13);
+            this.lblHora.TabIndex = 3;
+            this.lblHora.Text = "lblHora";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(17, 39);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(47, 13);
+            this.lblFecha.TabIndex = 2;
+            this.lblFecha.Text = "lblFecha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Hora";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Fecha";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnRegistrar);
+            this.groupBox3.Controls.Add(this.lblCosto);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.txtMinutos);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.cboHorario);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.cboTipo);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(12, 142);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(527, 100);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Datos de Llamada";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(422, 36);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lblCosto
+            // 
+            this.lblCosto.AutoSize = true;
+            this.lblCosto.Location = new System.Drawing.Point(344, 36);
+            this.lblCosto.Name = "lblCosto";
+            this.lblCosto.Size = new System.Drawing.Size(44, 13);
+            this.lblCosto.TabIndex = 7;
+            this.lblCosto.Text = "lblCosto";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(344, 16);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Costo";
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Location = new System.Drawing.Point(268, 32);
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(61, 20);
+            this.txtMinutos.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(265, 16);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Minutos";
+            // 
+            // cboHorario
+            // 
+            this.cboHorario.FormattingEnabled = true;
+            this.cboHorario.Items.AddRange(new object[] {
+            "Diurno (07:00-13:00)",
+            "Tarde (13:00-19:00)",
+            "Noche (19:00-23:00)",
+            "Madrugada(23:00-07:00)"});
+            this.cboHorario.Location = new System.Drawing.Point(136, 32);
+            this.cboHorario.Name = "cboHorario";
+            this.cboHorario.Size = new System.Drawing.Size(126, 21);
+            this.cboHorario.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(133, 16);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Horario de Llamada";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Local Nacional",
+            "Local Internacional",
+            "Movil Nacional",
+            "Movil Internacional"});
+            this.cboTipo.Location = new System.Drawing.Point(9, 32);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(121, 21);
+            this.cboTipo.TabIndex = 1;
+            this.cboTipo.SelectedIndexChanged += new System.EventHandler(this.cboTipo_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Tipo de Llamda";
+            // 
+            // lvRegistro
+            // 
+            this.lvRegistro.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvRegistro.GridLines = true;
+            this.lvRegistro.HideSelection = false;
+            this.lvRegistro.Location = new System.Drawing.Point(21, 261);
+            this.lvRegistro.Name = "lvRegistro";
+            this.lvRegistro.Size = new System.Drawing.Size(556, 97);
+            this.lvRegistro.TabIndex = 4;
+            this.lvRegistro.UseCompatibleStateImageBehavior = false;
+            this.lvRegistro.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TIPO DE LLAMADA";
+            this.columnHeader1.Width = 114;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "HORARIO";
+            this.columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "MINUTOS";
+            this.columnHeader3.Width = 64;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "COSTO POR MINUTO";
+            this.columnHeader4.Width = 127;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "COSTO POR LLAMADA";
+            this.columnHeader5.Width = 148;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 245);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(109, 13);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Registro de Llamadas";
+            // 
+            // btnEstadisticas
+            // 
+            this.btnEstadisticas.Location = new System.Drawing.Point(12, 364);
+            this.btnEstadisticas.Name = "btnEstadisticas";
+            this.btnEstadisticas.Size = new System.Drawing.Size(160, 23);
+            this.btnEstadisticas.TabIndex = 6;
+            this.btnEstadisticas.Text = "Mostrar Estadisticas";
+            this.btnEstadisticas.UseVisualStyleBackColor = true;
+            this.btnEstadisticas.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lvEstadisticas
+            // 
+            this.lvEstadisticas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvEstadisticas.GridLines = true;
+            this.lvEstadisticas.HideSelection = false;
+            this.lvEstadisticas.Location = new System.Drawing.Point(12, 393);
+            this.lvEstadisticas.Name = "lvEstadisticas";
+            this.lvEstadisticas.Size = new System.Drawing.Size(565, 97);
+            this.lvEstadisticas.TabIndex = 7;
+            this.lvEstadisticas.UseCompatibleStateImageBehavior = false;
+            this.lvEstadisticas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "CONDICION";
+            this.columnHeader6.Width = 307;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "VALOR";
+            this.columnHeader7.Width = 113;
+            // 
+            // tHora
+            // 
+            this.tHora.Tick += new System.EventHandler(this.tHora_Tick);
+            // 
+            // frmLlamdasSinValorRetornoConParametros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 553);
+            this.Controls.Add(this.lvEstadisticas);
+            this.Controls.Add(this.btnEstadisticas);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lvRegistro);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLlamdasSinValorRetornoConParametros";
+            this.Text = "Control de Registro de Lllamdas Call-Center";
+            this.Load += new System.EventHandler(this.frmLlamdasSinValorRetornoConParametros_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Label lblCosto;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cboHorario;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView lvRegistro;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnEstadisticas;
+        private System.Windows.Forms.ListView lvEstadisticas;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Timer tHora;
+    }
+}
+
